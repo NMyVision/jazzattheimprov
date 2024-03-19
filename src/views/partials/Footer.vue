757d2565962b62de4502deb9bd0a8906f4d31572
M. Gambill <mgambill@nmyvision.com>
2024-03-19 19:09:06 -0400

    update content

--- src/views/partials/Footer.vue
+++ src/views/partials/Footer.vue
@@ -3,13 +3,13 @@
     <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
       <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
         <div v-for="item in navigation.main" :key="item.name" class="pb-6">
-          <a :href="item.href" class="text-sm leading-6 text-gray-400 hover:text-gray-300">{{ item.name }}</a>
+          <a :href="item.href" class=" text-sm leading-6 text-gray-400 hover:text-gray-300">{{ item.name }}</a>
         </div>
       </nav>
       <div class="mt-10 flex justify-center space-x-10">
-        <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-200 hover:text-gray-50">
+        <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-200 hover:text-purple-600 group">
           <span class="sr-only">{{ item.name }}</span>
-          <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+          <component :is="item.icon" :class="['h-6 w-6 hover:scale-150 transition', item.color ]" aria-hidden="true" />
         </a>
       </div>
       <p class="mt-10 text-center text-xs leading-5 text-gray-500">&copy; 2020 Your Company, Inc. All rights reserved.</p>
@@ -22,17 +22,18 @@ import { defineComponent } from 'vue'
 
 const navigation = {
   main: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '#' },
-    { name: 'Accessibility', href: '#' },
-    { name: 'Sponsors', href: '#' }
+    // { name: 'About', href: '#' },
+    // { name: 'Blog', href: '#' },
+    // { name: 'Jobs', href: '#' },
+    // { name: 'Press', href: '#' },
+    // { name: 'Accessibility', href: '#' },
+    // { name: 'Sponsors', href: '#' }
   ],
   social: [
     {
       name: 'Facebook',
-      href: '#',
+      href: 'https://www.facebook.com/SMOOTHJAZZATTHEIMPROV',
+      color: 'hover:text-[#1877f2]',
       icon: defineComponent({
         render: () => (
           <svg fill="currentColor" viewBox="0 0 24 24">
@@ -43,7 +44,8 @@ const navigation = {
     },
     {
       name: 'Instagram',
-      href: '#',
+      href: 'https://instagram.com/smoothjazzattheimprov',
+      color: 'hover:text-[#405de6]',
       icon: defineComponent({
         render: () => (
           <svg fill="currentColor" viewBox="0 0 24 24">
@@ -56,21 +58,22 @@ const navigation = {
         )
       })
     },
-    {
-      name: 'X',
-      href: '#',
-      icon: defineComponent({
-        render: () => (
-          <svg fill="currentColor" viewBox="0 0 24 24">
-            <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
-          </svg>
-        )
-      })
-    },
+    // {
+    //   name: 'X',
+    //   href: '#',
+    //   icon: defineComponent({
+    //     render: () => (
+    //       <svg fill="currentColor" viewBox="0 0 24 24">
+    //         <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
+    //       </svg>
+    //     )
+    //   })
+    // },
 
     {
       name: 'YouTube',
-      href: '#',
+      href: 'https://www.youtube.com/@SmoothJazzattheImprovPresents',
+      color: 'hover:text-[#ff0000]',
       icon: defineComponent({
         render: () => (
           <svg fill="currentColor" viewBox="0 0 24 24">
--- src/views/partials/Mission.vue
+++ src/views/partials/Mission.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex items-center bg-black sm:h-96">
-    <div class="wrapper p-10 md:p-20">
+    <div class="wrapper p-10 md:p-20 max-w-screen-xl mx-auto">
       <h3 class="mb-2 text-2xl font-bold text-white">OUR MISSION</h3>
       <p class="leading-loose text-indigo-100">At Jazz at the Improv Society, our mission is twofold: to ensure that jazz never fades into obscurity and to provide a platform for both legendary and emerging jazz performers to captivate audiences with their musical prowess. Our society is committed to preserving the authenticity of jazz, fostering innovation within the genre, and passing on the rich legacy of jazz improvisation to future generations.</p>
     </div>
