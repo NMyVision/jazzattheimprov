7f27d62f5d83eb7a362f350cfb78500447265a96
M. Gambill <mgambill@nmyvision.com>
2024-04-04 02:09:40 -0400

    effort

--- src/views/partials/Carousel.vue
+++ src/views/partials/Carousel.vue
@@ -1,21 +1,38 @@
 <script setup lang="ts">
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/Carousel'
 import Autoplay from 'embla-carousel-autoplay'
+import { useToggle } from '@vueuse/core'
 // type Props = { }
 // const { } = defineProps<Props>()
 // const emits = defineEmits()
+
+const set = ['RCP_7326.jpg', 'RCP_7399.jpg', 'RCP_7372.jpg', 'RLC_2624.jpg', '2024_03/IMG_3781.JPG', '2024_03/IMG_3786.JPG', '2024_03/IMG_3788.JPG', '2024_03/IMG_3797.JPG', '2024_03/IMG_3806.JPG', '2024_03/IMG_3810.JPG', '2024_03/IMG_3842.JPG', '2024_03/IMG_3844.JPG', '2024_03/IMG_3858.JPG', '2024_03/IMG_3864.JPG', '2024_03/IMG_3868.JPG', '2024_03/IMG_3872.JPG', '2024_03/IMG_3880.JPG', '2024_03/IMG_3885.JPG', '2024_03/IMG_3887.JPG', '2024_03/IMG_3891.JPG']
+
+const randomizedSet = [...set]
+
+for (let i = randomizedSet.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1))
+  const temp = randomizedSet[i]
+  randomizedSet[i] = randomizedSet[j]
+  randomizedSet[j] = temp
+}
+
+
 </script>
 
 <template>
   <div class="relative isolate flex min-h-96 items-center md:h-[90dvh]">
     <div class="absolute inset-0 -z-10 bg-[url('/crowd-background.jpg')]"></div>
 
-    <Carousel class="relative mx-auto w-full max-w-screen-xl" :plugins="[Autoplay({ delay: 4000 })]">
+    <Carousel class="relative mx-auto w-full max-w-screen-xl" :plugins="[Autoplay({ delay: 5000 })]">
       <CarouselContent>
-        <CarouselItem class="aspect-video *:w-full *:object-cover"><img width="1280" height="720" src="\src\assets\RCP_7326.jpg" /></CarouselItem>
-        <CarouselItem class="aspect-video *:w-full *:object-cover"><img width="1280" height="720" src="\src\assets\RCP_7399.jpg" /></CarouselItem>
-        <CarouselItem class="aspect-video *:w-full *:object-cover"><img width="1280" height="720" src="\src\assets\RCP_7372.jpg" /></CarouselItem>
-        <CarouselItem class="aspect-video *:w-full *:object-cover"><img width="1280" height="720" src="\src\assets\RLC_2624.jpg" /></CarouselItem>
+        <template v-for="img in randomizedSet" :key="img">
+          <CarouselItem class="relative grid aspect-video justify-center *:max-h-[80dvh] *:w-full *:object-cover">
+            <div :style="`background-image: url('/src/assets/${img}')`" class="relative bg-contain bg-center bg-no-repeat">
+              <img :src="`/src/assets/${img}`" class="opacity-0" />
+            </div>
+          </CarouselItem>
+        </template>
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
---
+++ src/views/partials/Upcoming.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// type Props = { }
+// const { } = defineProps<Props>()
+// const emits = defineEmits()
+const data = {
+  name: 'Rick Braun',
+  date: 'April 17, 2024',
+  location: '1224 Parkside Main Street Cary, NC 27519'
+}
+const link = 'https://improv.com/raleigh/event/smooth+jazz+at+the+improv+presents%3a+rick+braun/13518773/'
+</script>
+
+<template>
+  <div class="flex-1 p-10">
+    <div class="flex justify-center gap-8">
+      <div class="mx-auto flex flex-col gap-10 md:flex-row">
+        <div class="md:w-2/5">
+          <img src="../../assets/RickBraun_20240417.jpeg" alt="Rick Braun" class="mx-auto h-full w-full max-w-screen-sm object-cover" />
+        </div>
+        <div class="space-y-4 text-xl text-purple-400 md:w-3/5">
+          <p class="uppercase">upcoming concert</p>
+
+          <h2 class="bg-gradient-to-r from-indigo-500 to-violet-500 bg-clip-text text-7xl font-light text-transparent">{{ data.name }}</h2>
+          <p class="text-yellow-500">
+            {{ data.date }}<br />
+            {{ data.location }}
+          </p>
+          <p class=""></p>
+          <div class="pt-6">
+            <a :href="link" target="_blank" class="block w-full rounded-md bg-gradient-to-tr from-indigo-700 to-violet-800 px-20 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 md:inline-block md:w-auto">Buy Ticket</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
