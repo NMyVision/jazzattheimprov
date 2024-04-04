f8ba3ee5dde9317979876650e0bf09ea8542b0f2
M. Gambill <mgambill@nmyvision.com>
2024-04-04 02:26:18 -0400

    move images

--- src/views/partials/Carousel.vue
+++ src/views/partials/Carousel.vue
@@ -6,7 +6,7 @@ import { useToggle } from '@vueuse/core'
 // const { } = defineProps<Props>()
 // const emits = defineEmits()
 
-const set = ['RCP_7326.jpg', 'RCP_7399.jpg', 'RCP_7372.jpg', 'RLC_2624.jpg', '2024_03/IMG_3781.JPG', '2024_03/IMG_3786.JPG', '2024_03/IMG_3788.JPG', '2024_03/IMG_3797.JPG', '2024_03/IMG_3806.JPG', '2024_03/IMG_3810.JPG', '2024_03/IMG_3842.JPG', '2024_03/IMG_3844.JPG', '2024_03/IMG_3858.JPG', '2024_03/IMG_3864.JPG', '2024_03/IMG_3868.JPG', '2024_03/IMG_3872.JPG', '2024_03/IMG_3880.JPG', '2024_03/IMG_3885.JPG', '2024_03/IMG_3887.JPG', '2024_03/IMG_3891.JPG']
+const set = ['RCP_7326.jpg', 'RCP_7399.jpg', 'RCP_7372.jpg', 'RLC_2624.jpg', '2024_03/IMG_3781.jpg', '2024_03/IMG_3786.jpg', '2024_03/IMG_3788.jpg', '2024_03/IMG_3797.jpg', '2024_03/IMG_3806.jpg', '2024_03/IMG_3810.jpg', '2024_03/IMG_3842.jpg', '2024_03/IMG_3844.jpg', '2024_03/IMG_3858.jpg', '2024_03/IMG_3864.jpg', '2024_03/IMG_3868.jpg', '2024_03/IMG_3872.jpg', '2024_03/IMG_3880.jpg', '2024_03/IMG_3885.jpg', '2024_03/IMG_3887.jpg', '2024_03/IMG_3891.jpg']
 
 const randomizedSet = [...set]
 
@@ -28,8 +28,8 @@ for (let i = randomizedSet.length - 1; i > 0; i--) {
       <CarouselContent>
         <template v-for="img in randomizedSet" :key="img">
           <CarouselItem class="relative grid aspect-video justify-center *:max-h-[80dvh] *:w-full *:object-cover">
-            <div :style="`background-image: url('/src/assets/${img}')`" class="relative bg-contain bg-center bg-no-repeat">
-              <img :src="`/src/assets/${img}`" class="opacity-0" />
+            <div :style="`background-image: url(/gallery/${img})`" class="relative bg-contain bg-center bg-no-repeat">
+              <img :src="`/gallery/${img}`" class="opacity-0" />
             </div>
           </CarouselItem>
         </template>
