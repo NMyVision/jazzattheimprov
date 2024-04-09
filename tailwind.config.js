d15d1042b620b8e5d5dc6d7d056fa077e8c0ad2f
M. Gambill <46201338+mgambill@users.noreply.github.com>
2024-04-09 00:47:40 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from "tailwindcss/defaultTheme"
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
